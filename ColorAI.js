67d34278e38bd21f468e8eea3af8f5d9b34b17ff
Andrew <andrey@b13design.ru>
2023-08-29 19:55:42 +0300

    Хуйня какаято

--- ColorAI.js
+++ ColorAI.js
@@ -1,34 +1,31 @@
 const tf = require('@tensorflow/tfjs-node-gpu');
 const { ManualInput, generateColor, saveModel, readDataset } = require("./dataGenerated");
 
-
-
-async function trainModel(inputs, targets, labels, epochs) {
+async function trainModel(inputs, targets, labels, epochs, batchSize) {
     const model = tf.sequential();
-    model.add(tf.layers.dense({ units: 64, inputShape: [3], activation: 'relu' }));
-    model.add(tf.layers.dense({ units: 32, activation: 'relu' }));
+    model.add(tf.layers.dense({ units: 32, inputShape: [3], activation: 'relu' }));
+    model.add(tf.layers.dropout({ rate: 0.5 }));
+    model.add(tf.layers.dense({ units: 16, activation: 'relu' }));
+    model.add(tf.layers.dropout({ rate: 0.5 }));
     model.add(tf.layers.dense({ units: labels.length, activation: 'softmax' }));
-    model.compile({ loss: 'categoricalCrossentropy', optimizer: 'adam' });
+    model.compile({ loss: 'categoricalCrossentropy', optimizer: tf.train.adam() });
 
     await model.fit(inputs, targets, {
         epochs: epochs,
-        callbacks: {
-            onEpochEnd: (epoch) => {
-                console.log(`Epoch ${epoch + 1}`);
-            }
-        }
+        batchSize: batchSize,
+        callbacks: tf.node.tensorBoard('logs'),
     });
 
     return model;
 }
 
-async function main(epochs) {
-
+async function main(epochs, batchSize) {
     const dataset = await readDataset();
     if (!dataset) {
         return;
     }
     const labels = [...new Set(dataset.map(item => item.name))];
+
     const labelToIndex = {};
     labels.forEach((label, index) => {
         labelToIndex[label] = index;
@@ -38,31 +35,28 @@ async function main(epochs) {
     const targets = [];
     dataset.forEach(data => {
         const { rgb, name } = data;
-        const input = rgb;
-        const target = labelToIndex[name];
-        inputs.push(input);
-        targets.push(target);
+        inputs.push(rgb);
+        targets.push(labelToIndex[name]);
     });
+
     const inputTensor = tf.tensor2d(inputs);
     const targetTensor = tf.oneHot(tf.tensor1d(targets, 'int32'), labels.length);
 
-    const model = await trainModel(inputTensor, targetTensor, labels, epochs);
+    const model = await trainModel(inputTensor, targetTensor, labels, epochs, batchSize);
 
-    // Сохранение модели
     await saveModel(model);
 
-    // Указываем цвет в ручную
     const manualColor = {
-        r: 251,
-        g: 206,
-        b: 177
+        r: 128,
+        g: 0,
+        b: 0
     };
 
-    // Автомотически сгенирированный массив, на случай если нужен рандомный цвет
     const autoColor = generateColor();
 
-    // Выдаем в консоль результат работы
     console.log(ManualInput(model, labels, manualColor));
 }
 
-main(Math.pow(2, 14));
+const epochs = 64;
+const batchSize = 32;
+main(epochs, batchSize);
